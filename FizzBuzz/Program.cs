403e9af8af04c20398ea0e6cc80f98f4f0962701
Hy Huynh <hyxhuynh.projects@gmail.com>
2024-04-17 13:44:06 -0700

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        /*
+         when the number is divisible by 3 return the word fizz
+
+         when the number is divisible by 5 return the word buzz
+
+         when the number is divisible by both, return the word fizzbuzz
+        */
+        public static string FizzBuzz (int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                return "FizzBuzz";
+            } 
+            else if (num % 3 == 0)
+            {
+                return "Fizz";
+            } 
+            else if (num % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return "Not divisible by 3 and/or 5";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{FizzBuzz(15)}"); // FizzBuzz
+            Console.WriteLine($"{FizzBuzz(9)}");  // Fizz
+            Console.WriteLine($"{FizzBuzz(10)}"); // Buzz
+            Console.WriteLine($"{FizzBuzz(4)}");  // Not divisible by 3 and/or 5
+        }
+    }
+}
